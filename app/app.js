bf343a858c582f460e6aa4f6c00b4bd5d38743a8
jingpei <breadthless@gmail.com>
2015-03-28 15:05:19 -0400

    working

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@
 	portfolioApp.config(function($routeProvider){
 		$routeProvider
 			.when('/portfolios', { controller : 'PortfolioListController', templateUrl : 'app/partials/portfolio_list_partial.html'})
-			.when('/portfolios/:portfolio_name', { controller : 'PortfolioViewController', templateUrl : 'app/partials/single_view_partial.html'})
+			.when('/portfolio/:portfolio_name', { controller : 'PortfolioViewController', templateUrl : 'app/partials/single_view_partial.html'})
 			.when('/', { redirectTo : '/portfolios' })
 			.otherwise({ redirectTo : '/404_page' });
 	});
--- app/controllers/portfolioviewcontroller.js
+++ app/controllers/portfolioviewcontroller.js
@@ -11,35 +11,23 @@ function PortfolioViewController( $scope, $routeParams ) {
 	var portfolios = { 
 
 	    "vessel1309": [
-	    { filename: "vessel1309-001.jpg",
-	    date: "2013/09/05",
-	    description: "I love this boat, so much booty." },
-	    { filename: "vessel1309-002.jpg",
-	    date: "2013/09/06",
-	    description: "We had a wonderful time on her." }
+	    { filename: "vessel1309-001.jpg", date: "2013/09/05", description: "I love this boat, so much booty." },
+	    { filename: "vessel1309-002.jpg", date: "2013/09/06", description: "We had a wonderful time on her." }
 	    ],
 
 	    "innocents1404": [
-	    { filename: "innocents1404-001.jpg",
-	    date: "2014/04/14",
-	    description: "So cold and so much sacking!" },
-	    { filename: "innocents1404-002.jpg",
-	    date: "2014/04/15",
-	    description: "The sails are so white here." }
+	    { filename: "innocents1404-001.jpg", date: "2014/04/14", description: "So cold and so much sacking!" },
+	    { filename: "innocents1404-002.jpg", date: "2014/04/15", description: "The sails are so white here." }
 	    ],
 
 	    "firstmate1210": [
-	    { filename: "firstmate1210-001.jpg",
-	    date: "2012/10/01",
-	    description: "Getting mah pipe on!" },
-	    { filename: "firstmate1210-002.jpg",
-	    date: "2012/10/02",
-	    description: "FTW!!!11!one!1" }
+	    { filename: "firstmate1210-001.jpg", date: "2012/10/01", description: "Getting mah pipe on!" },
+	    { filename: "firstmate1210-002.jpg", date: "2012/10/02", description: "FTW!!!11!one!1" }
 	    ]
 	};
 
 	if( portfolios[$scope.portfolio_name] ){
-		$scope.load_error_text = portfolios[$scope.portfolio_name];
+		$scope.portfolios = portfolios[$scope.portfolio_name];
 	}
 	else{
 		$scope.load_error_text = "Portfolio name doesn't exist";
